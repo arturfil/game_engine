97b494256bf228f62232ab114b93ee0bb1af4ada
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-05-24 16:16:36 -0400

    added the lua header in order to load lua scripts and build our game via lua scripts rather than by hard coding it ourselves, also started updating the loadgame method in game cpp file in order to load entities for the game

--- src/Game.cpp
+++ src/Game.cpp
@@ -66,47 +66,39 @@ void Game::Initialize(int width, int height) {
 Entity& player(manager.AddEntity("chopper", PLAYER_LAYER));
 
 void Game::LoadLevel(int levelNumber) {
-  // Start including new assets to assetmanager list
-  assetManager -> AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
-  assetManager -> AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
-  assetManager -> AddTexture("radar-image", std::string("./assets/images/radar.png").c_str()); 
-  assetManager -> AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
-  assetManager -> AddTexture("heliport-image", std::string("./assets/images/heliport.png").c_str());
-  assetManager -> AddTexture("projectile-image", std::string("./assets/images/bullet-enemy.png").c_str());
-  assetManager -> AddFont("charriot-font", std::string("./assets/fonts/charriot.ttf").c_str(), 14);  
+  sol::state lua;
+  lua.open_libraries(sol::lib::base, sol::lib::os, sol::lib::math);
 
-  map = new Map("jungle-tiletexture", 2, 32);
-  map -> LoadMap("./assets/tilemaps/jungle.map", 25, 20);
-  // Choper
-  player.AddComponent<TransformComponent>(240, 106, 0, 0, 32, 32, 1);
-  player.AddComponent<SpriteComponent>("chopper-image", 2, 90, true, false);
-  player.AddComponent<KeyboardControlComponent>("up", "right", "down", "left", "space");
-  player.AddComponent<ColliderComponent>("PLAYER", 240, 106, 32, 32);
+  std::string levelName = "Level" + std::to_string(levelNumber);
+  lua.script_file("./assets/scripts/" + levelName + ".lua");
 
-  Entity& tankEntity(manager.AddEntity("tank", ENEMY_LAYER));
-  tankEntity.AddComponent<TransformComponent>(150, 495, 5, 0, 32, 32, 1);
-  tankEntity.AddComponent<SpriteComponent>("tank-image");
-  tankEntity.AddComponent<ColliderComponent>("ENEMY", 150, 495, 32, 32);
+  sol::table levelData = lua[levelData];
+  sol::table levelAssets = levelData["assets"];
 
-  Entity& projectile(manager.AddEntity("projectile", PROJECTILE_LAYER));
-  projectile.AddComponent<TransformComponent>(150+16, 495+16, 0, 0, 4, 4, 1);
-  projectile.AddComponent<SpriteComponent>("projectile-image");
-  projectile.AddComponent<ColliderComponent>("PROJECTILE", 150+16, 495+16, 4, 4);
-  projectile.AddComponent<ProjectileEmitterComponent>(50, 270, 200, true);
+  unsigned int assetIndex = 0;
+  while (true) {
+    sol::optional<sol::table> existsAssetIndexNode = levelAssets[assetIndex];
+    if (existsAssetIndexNode == sol::nullopt) {
+      break;
+    } else {
+      sol::table asset = levelAssets[assetIndex];
+      std::string assetType = asset["type"];
+      if (assetType.compare("texture") == 0) {
+        std::string assetId = asset["id"];
+        std::string assetFile = asset["file"];
+        assetManager -> AddTexture(assetId, assetFile.c_str());
+      }
+    }
+    assetIndex++;
+  }
 
-  Entity& heliport (manager.AddEntity("Heliport", OBSTACLE_LAYER));
-  heliport.AddComponent<TransformComponent>(470, 420, 0, 0, 32, 32, 1);
-  heliport.AddComponent<SpriteComponent>("heliport-image");
-  heliport.AddComponent<ColliderComponent>("LEVEL_COMPLETE", 470, 420, 32, 32);
+  sol::table levelMap = levelData["map"];
+  std::string mapTextureId = levelMap["textureAssetId"];
+  std::string mapFile = levelMap["file"];
 
+  map = new Map(mapTextureId, static_cast<int>(levelMap["scale"]), static_cast<int>(levelMap["tileSize"]));
 
-
-  Entity& radarEntity(manager.AddEntity("Radar", UI_LAYER));
-  radarEntity.AddComponent<TransformComponent>(720, 15, 0, 0, 64, 64, 1);
-  radarEntity.AddComponent<SpriteComponent>("radar-image", 8, 150, false, true);
-  
-  Entity& labelLevelName(manager.AddEntity("LabelLevelName", UI_LAYER));
-  labelLevelName.AddComponent<TextLabelComponent>(10, 10, "First Level...", "charriot-font", WHITE_COLOR);
+  map -> LoadMap(mapFile, static_cast<int>(levelMap["mapSizeX"]), static_cast<int>(levelMap["mapSizeY"]));
 }
 
 void Game::ProcessInput() {
